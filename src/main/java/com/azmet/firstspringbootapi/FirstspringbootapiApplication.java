614775be3150b6979b97c153bb70df3f1c33e7d1
AndreforPhoenix <dre.m.williams@outlook.com>
2025-02-16 23:11:08 -0700

    updated sql query

--- src/main/java/com/azmet/firstspringbootapi/FirstspringbootapiApplication.java
+++ src/main/java/com/azmet/firstspringbootapi/FirstspringbootapiApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstspringbootapiApplication {
 
 	public static final String YEAR = "SELECT DISTINCT year FROM azmetdaily ORDER by year DESC";
-	public static final String MONTH = "SELECT DISTINCT monthname FROM azmetdaily ORDER by year DESC";
-	public static final String STATION = "SELECT DISTINCT station FROM azmetdaily ORDER by year DESC";
-	public static final String RESPONSE = "SHOW COLUMNS FROM azmetdaily ORDER by year DESC";
+	public static final String MONTH = "SELECT DISTINCT monthname FROM azmetdaily";
+	public static final String STATION = "SELECT DISTINCT station FROM azmetdaily ORDER by station DESC";
+	public static final String RESPONSE = "SHOW COLUMNS FROM azmetdaily";
 	public static final String DATA = "SELECT year, monthname, station, atmaxDegF, atminDegF, atmeanDegF, date FROM azmetdaily ORDER BY year DESC";
 
 	public static void main(String[] args) {
